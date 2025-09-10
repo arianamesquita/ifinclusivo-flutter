e99a08a2d13d965618e48c607b2389f5ef9f137b
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-09-09 23:24:03 -0300

    :recycle: refactor(About Napne) finalização da pagina mais adição de responsividade

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,12 +1,13 @@
 
 import 'package:go_router/go_router.dart';
+import 'package:if_inclusivo/routing/pages/about_routes/routes/about_napne_router.dart';
 import 'package:if_inclusivo/routing/pages/about_routes/routes/about_us_router.dart';
 import 'package:if_inclusivo/routing/pages/about_routes/shell_about_router.dart';
 import 'package:if_inclusivo/routing/pages/app/shell_app_router.dart';
 import 'package:if_inclusivo/routing/pages/auth/shell_auth_router.dart';
 
 GoRouter createRouter() => GoRouter(
-  initialLocation: AboutUsRoute().location,
+  initialLocation: AboutNapneRoute().location,
   routes: [
     $shellAboutRoute,
     $shellAuthRoute,
--- lib/ui/core/widgets/card_info.dart
+++ lib/ui/core/widgets/card_info.dart
@@ -8,44 +8,84 @@ class CardInfo extends StatelessWidget {
   final String label;
   final TextStyle? labelStyle;
   final double? maxWidth;
+  final double spacing;
+  final TextAlign textAlign;
+  final EdgeInsetsGeometry padding;
   const CardInfo({
-    super.key, required this.title, this.titleStyle, required this.label, this.labelStyle, this.maxWidth,
-
+    super.key,
+    required this.title,
+    this.titleStyle,
+    required this.label,
+    this.labelStyle,
+    this.maxWidth,
+    this.textAlign = TextAlign.center,
+    this.spacing = 37,
+    this.padding =const EdgeInsets.all(38),
   });
 
   @override
   Widget build(BuildContext context) {
     final deviceType = ResponsiveUtils.getDeviceType(context);
     final spacing = ResponsiveUtils.spacing(context);
-    final textScale =ResponsiveUtils.fontScale(context);
-    return
-    maxWidth == null ? buildComponent(deviceType, spacing, context, textScale):
-     ConstrainedBox(constraints: BoxConstraints(maxWidth: maxWidth!),
-      child: buildComponent(deviceType, spacing, context, textScale),
-    );
+    final textScale = ResponsiveUtils.fontScale(context);
+    return maxWidth == null
+        ? buildComponent(deviceType, spacing, context, textScale)
+        : ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxWidth!),
+          child: buildComponent(deviceType, spacing, context, textScale),
+        );
   }
 
-  Column buildComponent(DeviceScreenType deviceType, (double, double) spacing, BuildContext context, double textScale) {
+  Column buildComponent(
+    DeviceScreenType deviceType,
+    (double, double) spacings,
+    BuildContext context,
+    double textScale,
+  ) {
     return Column(
-      spacing: deviceType == DeviceScreenType.desktop? 37: spacing.$2,
+      spacing: deviceType == DeviceScreenType.desktop ? spacing : spacings.$2,
       children: [
-        Text(title, style: titleStyle?? Theme.of(context).textTheme.headlineSmall?.copyWith(
-          fontWeight: FontWeight.w600,
-          fontSize: (Theme.of(context).textTheme.headlineSmall?.fontSize?? 25)* textScale,
-          color: Theme.of(context).colorScheme.onSurface
-
-
-        ), softWrap: true),
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                title,
+                style:
+                    titleStyle ??
+                    Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      fontSize:
+                          (Theme.of(
+                                context,
+                              ).textTheme.headlineSmall?.fontSize ??
+                              25) *
+                          textScale,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                softWrap: true,
+                textAlign: textAlign,
+              ),
+            ),
+          ],
+        ),
         Card(
           color: Theme.of(context).colorScheme.onPrimary,
           child: Padding(
-            padding: EdgeInsets.all(38),
-            child: Text(label, style: labelStyle?? Theme.of(context).textTheme.titleLarge?.copyWith(
-              fontWeight: FontWeight.w500,
-              fontSize: (Theme.of(context).textTheme.titleLarge?.fontSize?? 20)* textScale,
-              color: Theme.of(context).colorScheme.inverseSurface
-
-            ), softWrap: true),
+            padding: padding,
+            child: Text(
+              label,
+              style:
+                  labelStyle ??
+                  Theme.of(context).textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.w500,
+                    fontSize:
+                        (Theme.of(context).textTheme.titleLarge?.fontSize ??
+                            20) *
+                        textScale,
+                    color: Theme.of(context).colorScheme.inverseSurface,
+                  ),
+              softWrap: true,
+            ),
           ),
         ),
       ],
