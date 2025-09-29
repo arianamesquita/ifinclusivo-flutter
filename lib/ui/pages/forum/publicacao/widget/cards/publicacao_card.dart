2233da7db6edfa4eb09c8dcc6ae3d94bdee9dbc4
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-09-29 18:33:04 -0300

    :recycle: refactor(Publicação card) Ajuste na interface

--- lib/ui/pages/forum/publicacao/widget/cards/publicacao_card.dart
+++ lib/ui/pages/forum/publicacao/widget/cards/publicacao_card.dart
@@ -15,49 +15,61 @@ class PublicacaoCard extends StatelessWidget {
     return ConstrainedBox(
       constraints: BoxConstraints(maxWidth: 800),
       child: Card(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AccountHeader.mine(
-              nameUser: model.usuario.nome,
-              imgPath: null,
-              isAnswerAccepted: false,
-              canMarkAsAnswer: false,
-              menuItems: [
-                PopupMenuItem(
-                  value: "denuciar",
-                  child: Text("Denuciar"),
-                  onTap: () {
-                    // callback editar
-                    print("Editar publicação");
-                  },
+        clipBehavior: Clip.antiAlias,
+        child: InkWell(
+          onTap:()=> PublicacaoRouter(model.id).go(context),
+          hoverColor: Colors.transparent,
+          focusColor: Colors.transparent,
+          mouseCursor: SystemMouseCursors.grab,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: AccountHeader.mine(
+                  nameUser: model.usuario.nome,
+                  imgPath: null,
+                  isAnswerAccepted: false,
+                  canMarkAsAnswer: false,
+                  menuItems: [
+                    PopupMenuItem(
+                      value: "denuciar",
+                      child: Text("Denuciar"),
+                      onTap: () {
+                        // callback editar
+                        print("Editar publicação");
+                      },
+                    ),
+                  ],
+                  onUnmarkAnswer: () {}, onMarkAnswer: () {  },
+                  dataCriacao: model.dataCriacao,
                 ),
-              ],
-              onUnmarkAnswer: () {}, onMarkAnswer: () {  },
-              dataCriacao: model.dataCriacao,
-            ),
-            ContentCard(
-              title: model.titulo,
-              contentJson:model.texto),
-            ListChipsCard(categorias: model.categorias),
+              ),
+              ContentCard(
+                title: model.titulo,
+                contentJson:model.texto,
+                onPressed:()=> PublicacaoRouter(model.id).go(context),
+              ),
+              ListChipsCard(categorias: model.categorias),
 
-            BottomBarPublication(
-              likes: model.totalLikes,
-              comments: model.totalRespostas,
-              isLiked: model.curtidoPeloUsuario, // já curtido pelo usuário
-              onLike: () {
-                // chamada ao backend (async, mas UI já mudou)
-                print("like/unlike API");
-              },
-              onComment: () {
-                PublicacaoRouter('1').go(context);
-              },
-              onShare: () {
-                print("abrir opções de compartilhamento");
-              },
-            ),
-          ],
+              BottomBarPublication(
+                likes: model.totalLikes,
+                comments: model.totalRespostas,
+                isLiked: model.curtidoPeloUsuario, // já curtido pelo usuário
+                onLike: () {
+                  // chamada ao backend (async, mas UI já mudou)
+                  print("like/unlike API");
+                },
+                onComment: () {
+                  PublicacaoRouter(model.id).go(context);
+                },
+                onShare: () {
+                  print("abrir opções de compartilhamento");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
