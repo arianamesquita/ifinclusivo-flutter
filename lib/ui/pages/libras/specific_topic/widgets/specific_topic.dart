17d8e8cde9ad3e75de5ba7c660149abf0f5fd81f
Davi Antonio <adavi9920@gmail.com>
2025-10-16 20:35:54 -0300

    fix: corrigindo bug da thumbnail nos cards de topico especifico.

--- lib/ui/pages/libras/specific_topic/widgets/specific_topic.dart
+++ lib/ui/pages/libras/specific_topic/widgets/specific_topic.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:if_inclusivo/ui/core/animations/hover_effect.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 import '../../../../../utils/text_formater.dart';
 
-class SpecificTopic extends StatelessWidget {
+class SpecificTopic extends StatefulWidget {
   final String title;
-  final String urlVideo;
   final String description;
   final GestureTapCallback onTap;
 
   const SpecificTopic({
     super.key,
     required this.title,
-    required this.urlVideo,
     required this.description,
     required this.onTap,
   });
 
   @override
+  State<SpecificTopic> createState() => _SpecificTopicState();
+}
+
+class _SpecificTopicState extends State<SpecificTopic> {
+  late YoutubePlayerController _controller;
+  String videoId = YoutubePlayerController.convertUrlToId("https://www.youtube.com/watch?v=F3TiMx-zG-A")!;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController.fromVideoId(
+      videoId: videoId,
+      autoPlay: false,
+      params: const YoutubePlayerParams(
+        mute: true,
+        showControls: true,
+        showFullscreenButton: true,
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String palavra = formatarTexto(title);
+    String palavra = formatarTexto(widget.title);
 
     return HoverEffect(
       child: Material(
@@ -27,7 +48,7 @@ class SpecificTopic extends StatelessWidget {
         borderRadius: BorderRadius.circular(40),
         child: InkWell(
           borderRadius: BorderRadius.circular(40),
-          onTap: onTap,
+          onTap: widget.onTap,
           child: Container(
             width: 387,
             height: 361,
@@ -36,12 +57,18 @@ class SpecificTopic extends StatelessWidget {
               color: Color.fromRGBO(200, 255, 192, 1),
             ),
             child: Padding(
-              padding: const EdgeInsets.all(37),
+              padding: const EdgeInsets.only(left: 50, right: 50),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset(urlVideo, fit: BoxFit.cover),
-                  SizedBox(height: 21),
+                  Image.network(
+                    YoutubePlayerController.getThumbnail(
+                      videoId: videoId,
+                      quality: ThumbnailQuality.high,
+                      webp: false,
+                    ), fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 15),
                   Text(
                     textAlign: TextAlign.center,
                     palavra,
@@ -51,10 +78,10 @@ class SpecificTopic extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 14),
+                  SizedBox(height: 5),
                   Text(
                     textAlign: TextAlign.center,
-                    description,
+                    widget.description,
                     style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                       color: Theme.of(context).colorScheme.onSurface,
                     ),
