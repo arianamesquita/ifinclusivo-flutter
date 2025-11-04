99e115e9caafc0a9b85f78daf5c680795a211c69
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-11-04 19:22:28 -0300

    :recycle: refactor(gen code) ajustes em classes problematicas

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -6,7 +6,6 @@ import 'package:if_inclusivo/ui/exceptions/not_found_404.dart';
 import 'package:if_inclusivo/ui/pages/auth/reset_password/reset_password_page.dart';
 import 'package:if_inclusivo/ui/pages/auth/token/token_page.dart';
 import 'package:if_inclusivo/ui/pages/forum/feed/viewmodels/feed_viewmodel.dart';
-import 'package:if_inclusivo/ui/pages/libras/midia_page.dart';
 import 'package:if_inclusivo/ui/pages/libras/specific_topic/specific_topic_page.dart';
 import 'package:if_inclusivo/ui/pages/profile/account_security/account_security_page.dart';
 import 'package:if_inclusivo/ui/pages/profile/profile_page.dart';
