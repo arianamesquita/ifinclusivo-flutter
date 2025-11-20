e0331a344453b124b682abf21bdbb46a57bc1a40
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-11-20 18:03:30 -0300

    :sparkles: feet(deep links) adição de deep link

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:go_router/go_router.dart';
+import 'package:if_inclusivo/routing/app_router.dart';
 import 'package:if_inclusivo/ui/core/theme/theme.dart';
 import 'package:if_inclusivo/ui/core/theme/util.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'config/dependencies.dart';
+import 'package:app_links/app_links.dart';
 
-class MyApp extends StatelessWidget {
+
+
+
+
+class MyApp extends StatefulWidget {
   final SharedPreferences sharedPreferences;
 
   const MyApp({
@@ -17,15 +23,41 @@ class MyApp extends StatelessWidget {
   });
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final AppLinks _appLinks;
+
+  @override
+  void initState() {
+    super.initState();
+    _appLinks = AppLinks();
+    _appLinks.getInitialLink().then(_handleDeepLink);
+    _appLinks.uriLinkStream.listen(_handleDeepLink);
+  }
+
+  void _handleDeepLink(Uri? uri) {
+    if (uri == null) return;
+    final router = GoRouter.of(context);
+    if (uri.path == "/auth/verify-email") {
+      final token = uri.queryParameters["token"];
+      TokenValidateRouter(token: token).push(context);
+
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     TextTheme textTheme = createTextTheme(context, "Poppins", "Poppins");
     MaterialTheme theme = MaterialTheme(textTheme);
 
     return MultiProvider(
-      providers: providers(sharedPreferences),
+      providers: providers(widget.sharedPreferences),
       child: Builder(
         builder: (context) {
           final router = context.watch<GoRouter>();
+
           return MaterialApp.router(
             title: 'IF Inclusivo',
             theme: theme.light(),
@@ -38,7 +70,8 @@ class MyApp extends StatelessWidget {
               FlutterQuillLocalizations.delegate,
             ],
             supportedLocales: const [
-              Locale('pt', 'BR'),],
+              Locale('pt', 'BR'),
+            ],
           );
         },
       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import file_selector_macos
 import flutter_inappwebview_macos
 import path_provider_foundation
@@ -16,6 +17,7 @@ import video_player_avfoundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
